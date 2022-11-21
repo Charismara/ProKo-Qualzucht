cbe11c3051d9f2536b8e84467ab5f1714084aded
Christian Raoulis <craoulis@googlemail.com>
2022-11-21 15:09:56 +0100

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,122 @@
+'use client';
 import './globals.css'
+import {Disclosure, Menu, Transition} from "@headlessui/react";
+import {Fragment} from "react";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en" className={"h-full"}>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head>
-          <title>Proko 2022</title>
-      </head>
-      <body className={"h-full"}>
-      {children}
-      </body>
-    </html>
-  )
+const navigation = [
+    {name: 'Dashboard', href: '#', current: true},
+    {name: 'Team', href: '#', current: false},
+    {name: 'Projects', href: '#', current: false},
+    {name: 'Calendar', href: '#', current: false},
+]
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function RootLayout({children,}: { children: React.ReactNode }) {
+    return (
+        <html lang="en" className={"h-full"}>
+        <body className={"h-full bg-gray-900 text-white"}>
+        <div className="min-h-full">
+            <Disclosure as="nav" className="border-b border-black">
+                {({open}) => (
+                    <>
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                            <div className="flex h-16 justify-between">
+                                <div className="flex">
+                                    <div className="flex flex-shrink-0 items-center">
+                                        <img
+                                            className="block h-8 w-auto lg:hidden"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=white"
+                                            alt="Proko 2023 - Qualzucht"
+                                        />
+                                        <img
+                                            className="hidden h-8 w-auto lg:block"
+                                            src="https://tailwindui.com/img/logos/mark.svg?color=white"
+                                            alt="Proko 2023 - Qualzucht"
+                                        />
+                                    </div>
+                                    <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current
+                                                        ? 'border-indigo-500'
+                                                        : 'border-transparent text-gray-300 hover:border-gray-300 hover:text-gray-100',
+                                                    'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                                    {/* Profile dropdown */}
+                                    <Menu as="div" className="relative ml-3">
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-200"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                        </Transition>
+                                    </Menu>
+                                </div>
+                                <div className="-mr-2 flex items-center sm:hidden">
+                                    {/* Mobile menu button */}
+                                    <Disclosure.Button
+                                        className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                        <span className="sr-only">Open main menu</span>
+                                        {open ? (
+                                            <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
+                                        ) : (
+                                            <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
+                                        )}
+                                    </Disclosure.Button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Disclosure.Panel className="sm:hidden">
+                            <div className="space-y-1 pt-2 pb-3">
+                                {navigation.map((item) => (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current
+                                                ? 'bg-indigo-50 border-indigo-500 text-indigo-700'
+                                                : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-slate-300 hover:text-gray-800',
+                                            'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                ))}
+                            </div>
+                        </Disclosure.Panel>
+                    </>
+                )}
+            </Disclosure>
+
+            <div className="py-10">
+                <div className={"bg-gray-700 mx-auto max-w-7xl rounded-xl"}>
+                    {children}
+                </div>
+            </div>
+        </div>
+        </body>
+        </html>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,131 +1,69 @@
 'use client'
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-]
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
+import Image from "next/image";
+import QualzuchtHunde from 'public/Hunde-Qualzuchten.png'
+import QualzuchtKatzen from 'public/Katzen-Qualzuchten.png'
 
 export default function Example() {
-  return (
-    <>
-      <div className="min-h-full">
-        <Disclosure as="nav" className="border-b border-gray-200 bg-white">
-          {({ open }) => (
-            <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 justify-between">
-                  <div className="flex">
-                    <div className="flex flex-shrink-0 items-center">
-                      <img
-                        className="block h-8 w-auto lg:hidden"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                      />
-                      <img
-                        className="hidden h-8 w-auto lg:block"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                      />
-                    </div>
-                    <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'border-indigo-500 text-gray-900'
-                              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                            'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                  </div>
-                  <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                    {/* Profile dropdown */}
-                    <Menu as="div" className="relative ml-3">
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
-                  </div>
-                  <div className="-mr-2 flex items-center sm:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
-                  </div>
+    return (
+        <>
+            <header>
+                <div className="px-4 sm:px-6 lg:px-8 text-center">
+                    <h1 className="text-7xl font-bold leading-tight tracking-tight">Das Problem Qualzucht</h1>
                 </div>
-              </div>
+            </header>
+            <main className={"mt-8"}>
+                <div className="sm:px-6 lg:px-8">
+                    <div className="px-4 py-8 sm:px-0 text-xl flex">
+                        <div className={"w-full md:w-1/2 pr-4"}>
+                            <p className={"mb-4"}>Qualzucht ist ein weit verbreitetes Wort, dieses Wort definiert die Zucht von Tieren, die mit Fehlbildungen und anderen Gesundheitlichen Schäden und Schmerzen aus wirtschaftlichen Gründen in
+                                                  Kauf genommen werden.</p>
+                            <p className={"mb-4"}>Jedoch gibt es auch Rassen die absichtlich so gezüchtet werden, weil die Menschen dieses aussehen „Hübsch“ finden und die Tiere müssen dann darunter leiden.</p>
+                            <p>Die Elterntiere werden nach genauen Maßgaben herausgesucht und zur Zucht verwendet. Ihre Nachkommen haben dann meistens die gewünschten Merkmale.</p>
+                        </div>
+                        <div className={"w-1/2 hidden md:flex justify-center"}>
+                            <div className="group">
+                                <a href={"https://www.mein-haustier.de/wp-content/uploads/2022/07/Hunde-Qualzuchten.png"}>
+                                    <Image
+                                        className={"lg:w-2/3 object-scale-down object-center mx-auto"}
+                                        src={QualzuchtHunde} alt={"https://www.mein-haustier.de/wp-content/uploads/2022/07/Hunde-Qualzuchten.png"}
+                                        style={{objectFit: "scale-down"}}
+                                    />
+                                    <h3 className="mt-3 text-sm text-gray-500 text-center">
+                                        https://www.mein-haustier.de/wp-content/uploads/2022/07/Hunde-Qualzuchten.png
+                                    </h3>
+                                </a>
+                            </div>
+                        </div>
 
-              <Disclosure.Panel className="sm:hidden">
-                <div className="space-y-1 pt-2 pb-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? 'bg-indigo-50 border-indigo-500 text-indigo-700'
-                          : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                        'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
+                    </div>
                 </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-
-        <div className="py-10">
-          <header>
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Dashboard</h1>
-            </div>
-          </header>
-          <main>
-            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-              {/* Replace with your content */}
-              <div className="px-4 py-8 sm:px-0">
-                <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-              </div>
-              {/* /End replace */}
-            </div>
-          </main>
-        </div>
-      </div>
-    </>
-  )
+                <div className="sm:px-6 lg:px-8">
+                    <div className="px-4 py-8 sm:px-0 text-xl flex">
+                        <div className={"w-1/2 hidden md:flex justify-center"}>
+                            <div className="group">
+                                <a href={"https://www.mein-haustier.de/wp-content/uploads/2022/07/Katzen-Qualzuchten.png"}>
+                                    <Image
+                                        className={"lg:w-2/3 object-scale-down object-center mx-auto"}
+                                        src={QualzuchtKatzen} alt={"https://www.mein-haustier.de/wp-content/uploads/2022/07/Katzen-Qualzuchten.png"}
+                                        style={{objectFit: "scale-down"}}
+                                    />
+                                    <h3 className="mt-3 text-sm text-gray-500 text-center">
+                                        https://www.mein-haustier.de/wp-content/uploads/2022/07/Katzen-Qualzuchten.png
+                                    </h3>
+                                </a>
+                            </div>
+                        </div>
+                        <div className={"w-full md:w-1/2 pl-4"}>
+                            <p className={"mb-4"}>Qualzucht bezieht sich nicht nur auf Hunde, sondern auch auf Katzen und Kleintiere.</p>
+                            <p className={"mb-4"}>Eigentlich alles was der Mensch züchten kann, es gibt unzählige Tierarten und Rassen mit gezüchteter Fehlbildung oder sonstige Qualzuchten, denn der Mensch formt sich das Tier so, wie es ihm
+                                                  gefällt.</p>
+                            <p className={"mb-4"}>Sei es Groß oder Klein, Große Ohren oder hängende Ohren. Plattes Gesicht oder gar ein komplett Nacktes Tier. </p>
+                            <p>Jedes Tier welches verzüchtet wird hat seine vielen kleinen oder großen Leiden die ihnen viele Probleme bereiten.</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
