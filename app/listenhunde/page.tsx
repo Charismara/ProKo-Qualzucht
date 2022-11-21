6eff848fb7b2f5e829b0c169bc7a5a763c04646e
Christian Raoulis <craoulis@googlemail.com>
2022-11-21 18:07:04 +0100

    finish listenhunde page.tsx

---
+++ app/listenhunde/page.tsx
@@ -0,0 +1,131 @@
+'use client'
+
+import Eigenes from 'public/WhatsApp Bild 2022-11-21 um 17.02.45.jpg';
+import Costa from 'public/WhatsApp Bild 2022-11-21 um 17.15.30.jpg';
+import Keil from 'public/7231f4cf3779e78915ab3287093343_original.jpg';
+import Bull from 'public/kranker-hund-large.jpg';
+import Image from "next/image";
+
+
+export default function Example() {
+    return (
+        <>
+            <main>
+                <div className="mx-auto max-w-7xl py-8 px-4 sm:py-8 sm:px-6 lg:px-8">
+                    <div className="text-center">
+                        <p className="mt-1 text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                            Listenhund Züchtungen
+                        </p>
+                        <div className="mx-auto mt-8 max-w-4xl text-xl">
+                            <div className={"mt-8"}>
+                                <p>Viele Menschen stellen sich eine bestimmte Größe bei ihrem Hund vor, und wenn diese noch nicht existiert, dann Züchten wir sie halt.</p>
+                                <br/>
+                                <p>Ein Problem, wenn man einen Hund groß Züchtet ist, dass diese sehr häufig Probleme mit den Knochen haben und der Körper so schwer ist, dass mit der Zeit Liegeschwielen entstehen.</p>
+                                <br/>
+                                <p>Große XL-XXL Hunde sind auch körperlich nicht wirklich belastbar wie kleinere Hunde da diese nicht so
+                                   schwere Knochen und Muskelmasse mit sich schleppen müssen.</p>
+                                <br/>
+                                <p>Für Viele Menschen ist ein Hund schön je größer er ist, über die Körperlichen leiden machen sich die Menschen keine Gedanken.</p>
+                                <br/>
+                                <div className="group">
+                                    <Image src={Eigenes} alt={""} style={{objectFit: "scale-down"}} className={"mx-auto w-1/2"}/>
+                                    <h3 className="mt-3 text-sm text-gray-500 text-center">
+                                        Eigenes Bild
+                                    </h3>
+                                </div>
+                                <p className="mt-1 text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                                    American Bully XL
+                                </p>
+                                <br/>
+                                <p>Man sieht häufig <span className={"font-bold"}>American Bully XL</span> Hunde, sie sehen aus wie Body-Builder Hunde und als könnten sie dich jeden Moment fertig machen. Doch diese Hunde sehen meistens nur
+                                   so aus, weil sie so gezüchtet wurden.</p>
+                                <br/>
+                                <p>Meistens sind sie sehr liebe Tiere, jedoch leidet diese Rasse häufig an Allergien welche auch häufig zu Fell Problemen führen oder sehr häufig zu Augenentzündungen.</p>
+                                <br/>
+                                <div className="group">
+                                    <Image src={Costa} alt={""} style={{objectFit: "scale-down"}} className={"mx-auto w-1/2"}/>
+                                    <h3 className="mt-3 text-sm text-gray-500 text-center">
+                                        Eigenes Bild
+                                    </h3>
+                                </div>
+                                <p>Häufig Leiden sie auch unter einem Keilwirbel dies bedeutet eine Wirbelkörperdeformation diese zeigen Ventral (Zum Bauch hin) betonten Höhenminderung des Wirbelkörpers einhergeht.</p>
+                                <br/>
+                                <p>Diese Deformation wird häufig bei Rottweilern, Westhighland White Terriern, Foxterriern, Yorkshire Terriern, Beagles und Deutsch-Kurzhaar festgestellt.</p>
+                                <br/>
+                                <p> Ein Keilwirbel hat meistens folgende Symptome, Bewegungs- und Koordinationsstörungen dazu folgen dann auch ab und zu Lähmungen.</p>
+                                <br/>
+                                <p>Häufig treten auch verminderte Reflexe auf sowie vermindertes Stehvermögen und Muskelatrophie der Hintergliedmaßen. Natürlich haben die Hunde auch an dem betroffenen wirbelabschnitte Schmerzen, die die
+                                   Hunde sehr plagen.</p>
+                                <br/>
+                                <p>Leider ist auch ein Symptom, dass die Hunde Inkontinent sind sowohl beim Kot als auch beim Urin.</p>
+                                <br/>
+                                <div className="group">
+                                    <a href={"https://dccdn.de/www.doccheck.com/data/dr/sv/gy/85/mb/le/7231f4cf3779e78915ab3287093343_original.jpg"}>
+                                        <Image src={Keil} alt={""} style={{objectFit: "scale-down"}} className={"mx-auto w-1/2"}/>
+                                        <h3 className="mt-3 text-sm text-gray-500 text-center">
+                                            https://dccdn.de/www.doccheck.com/data/dr/sv/gy/85/mb/le/7231f4cf3779e78915ab3287093343_original.jpg
+                                        </h3>
+                                    </a>
+                                </div>
+                                <p>Auch wie bei vielen anderen Hunden, haben manche American Bully’s eine viel zu kurze Nase und leiden deshalb auch häufig unter Atemproblemen und können auch deshalb nicht sehr viel Laufen.</p>
+                                <br/>
+                                <p>Manche Bully’s haben auch eine Fehlstellung des Augenlides, hier rollt sich das Augenlid nach innen. Dies kann schmerzhaft sein und die Hornhaut beschädigen.</p>
+                                <br/>
+                                <p>Alles in allem sind American Bully’s XL sehr geschädigte Tiere, immer viel zu viele Muskeln und sehr viele Körperlichen Probleme.</p>
+                                <p className="mt-1 text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                                    Miniatur Bullterrier
+                                </p>
+                                <br/>
+                                <p>Der Miniatur Bullterrier leidet auch unter vielen Rassendispositionen für viele Erbkrankheiten ein großes Thema welches nicht zu übersehen ist, ist das Gesicht vom Bullterrier, sowohl beim normal großen
+                                   und beim Mini.</p>
+                                <br/>
+                                <p>Der kleine Bullterrier hat auch viel zu kurze beine welche ihm große Probleme bereiten kann, zum Beispiel eine Patellaluxation, hierbei verschiebt sich durch die zu kurzen beine häufig die Kniescheibe.</p>
+                                <br/>
+                                <div className="group">
+                                    <a href={"https://www.mini-bull-terrier.de/media/images/kranker-hund-large.jpg"}>
+                                        <Image src={Bull} alt={""} style={{objectFit: "scale-down"}} className={"mx-auto w-3/4"}/>
+                                        <h3 className="mt-3 text-sm text-gray-500 text-center">
+                                            https://www.mini-bull-terrier.de/media/images/kranker-hund-large.jpg
+                                        </h3>
+                                    </a>
+                                </div>
+                                <br/>
+                                <p>Dazu leidet diese Rasse auch unter der Primären Linsenluxation, diese Krankheit kann zur Erblindung führen und ist sehr schmerzhaft. Bei dieser Erkrankung reißen die Zonulafasern nach und nach, die Linse
+                                   löst sich aus ihrer Position und „Schwimmt“ frei im Auge herum. Bei dieser Krankheit tränt das Auge sehr häufig und der Hund kneift häufig die Augen zu. Wenn man genauer hinsieht, kann man auch sehen, dass
+                                   die Hornhaut des Auges leicht getrübt erscheint.</p>
+                                <br/>
+                                <p>Ebenso kann ein Bullterrier an einer Aortenstenose erkranken. Hier ist die Hauptschlagader in der linken Herzhälfte verengt und damit muss das Herz mehr pumpen, um die Verengung zu überwinden. Durch das
+                                   ständige Pumpen vergrößern sich die Muskelzellen und dadurch nimmt der Durchmesser in der Herzkammer ab. Deswegen kommt es zu einer Herzrhythmusstörung. Der Bullterrier ist dann schlechter belastbar und
+                                   bei hoher Belastung wird der Hund ohnmächtig. Atemnot ist auch häufig ein Symptom.</p>
+                                <br/>
+                                <p>Dieser Hund hat ebenso in seiner Rasse die Erbkrankheit der Taubheit. Dies passiert, wenn Anteile des Innenohrs Degenerieren. Aber bei dieser Krankheit kann der Hund trotzdem ein erfülltes Leben
+                                   führen.</p>
+                                <br/>
+                                <p>Auch Bullterrier haben viele Allergien die sie plagen. Meistens leiden aber die Weißen Bullterrier mehr unter Beschwerden als andere dieser Rasse, da diese einen Gendefekt haben denn weiß ist keine
+                                   natürliche fellgebung bei Hunden. Häufig leiden diese Hunde dann unter Juckreiz, Ohrenentzündungen und sehr oft Durchfall.</p>
+                                <br/>
+                                <p>Leider haben Bullterrier auch Rassebedingt oft Zwischenzehenzysten diese schmerzen und jucken sehr oft weshalb sie sich die kleinen Bläschen einfach aufbeißen und so die Wunden sich entzünden und noch mehr
+                                   schmerzen und jucken.</p>
+                                <br/>
+                                <div className={"w-full border-t-4 border-gray-900"}/>
+                                <br/>
+                                <p>Oft haben Listenhunde auch Verhaltensauffälligkeiten, weil die Menschen nicht darauf achten welche Hunde sie miteinander züchten um ihre Zielzüchtung zu erreichen. Da kann es auch mal sein, dass die
+                                   Listenhunde die eh schon verzüchtet sind auch noch mit einem Hund gezüchtet werden der starke Verhaltens Auffälligkeiten hat und seine nachfahren führen dann vielleicht das Verhalten in ihren Genen
+                                   weiter.</p>
+                                <br/>
+                                <p>Die Problematik bei Listenhunden ist, dass sie immer im Trend sein werden und deshalb immer weiter mit ihnen gezüchtet wird auch wenn es schon bei vielen listen Hunden verboten wird. Viele Hunde werden
+                                   gezüchtet, weil sie „Süß“ sind aber niemand achtet meist auf die Verhaltens Auffälligkeiten die bei vielen Listenhunden Vorhanden sind oder die ganzen Erbkrankheiten die in diesen Züchtungen schon
+                                   vorhanden sind. Viele Erbkrankheiten sind auch da, weil die meisten Hunde wie American Bully aus der Inzest Zucht entstanden sind und hier immer dieselben gendefekte weitervererbt werden.</p>
+                                <br/>
+                                <p>Qualzuchten sollten nicht unterstützt werden. Solche Züchter sollten generell nicht unterstützt werden also lieber Adoptieren statt Züchter zu unterstützen. Dann geht die Nachfrage runter und die Züchter
+                                   achten vielleicht mehr auf die Lebensqualität der gezüchteten Hunde. </p>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
--- app/quellen/page.tsx
+++ app/quellen/page.tsx
@@ -1,9 +1,5 @@
 'use client'
 
-import Image from "next/image";
-import QualzuchtHunde from 'public/Hunde-Qualzuchten.png'
-import QualzuchtKatzen from 'public/Katzen-Qualzuchten.png'
-
 export default function Example() {
     return (
         <>
@@ -19,6 +15,12 @@ export default function Example() {
                     <QuellenListElement link={"https://www.svk-asmpa.ch/images/aktuell/2018/2018-debut-campagne-contre-le-nez-plat-excessif-chez-le-chien.jpg"}/>
                     <QuellenListElement link={"https://www.uni-giessen.de/fbz/fb10/institute_klinikum/klinikum/kleintierklinik/Chirurgie/neurologie/Patienteninformation/c/chiari-malformation"}/>
                     <QuellenListElement link={"https://www.tierschutzbund.de/information/hintergrund/heimtiere/qualzucht/"}/>
+                    <QuellenListElement link={"https://www.mini-bull-terrier.de/gesundheit.html"}/>
+                    <QuellenListElement link={"https://www.doccheck.com/de/detail/photos/2106-keilwirbel-kongenitale-skoliose"}/>
+                    <QuellenListElement link={"https://www.tierarzt-mg.de/augenheilkunde/augenerkrankungen.html "}/>
+                    <QuellenListElement link={"https://www.gesunde-bulldoggen.de/laufen/keilwirbel.html"}/>
+                    <QuellenListElement link={"https://de.wikipedia.org/wiki/Keilwirbel "}/>
+                    <QuellenListElement link={"https://www.tiho-hannover.de/kliniken-institute/institute/institut-fuer-tierzucht-und-vererbungsforschung/forschung/forschungsprojekte-hund/keilwirbel "}/>
                     <li>Eigenes Wissen</li>
                 </ul>
             </main>
